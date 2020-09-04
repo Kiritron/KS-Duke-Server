fccedbccd1c6c5e1613a0883b31db467930267ea
Kiritron Stablecore <kiritron@kiritron.space>
2020-09-04 12:51:01 +0300

    Исправлена мелкая ошибка

--- src/space/kiritron/duke_srv/core/Server.java
+++ src/space/kiritron/duke_srv/core/Server.java
@@ -1,13 +1,13 @@
 package space.kiritron.duke_srv.core;
 
 import space.kiritron.duke_srv.ks_libs.pixel.filefunc.GetPathOfAPP;
+import space.kiritron.duke_srv.ks_libs.pixel.logger.genLogMessage;
+import space.kiritron.duke_srv.ks_libs.pixel.logger.toConsole;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static space.kiritron.duke_srv.core.console.writeToConsole;
-import static space.kiritron.duke_srv.init.NAME_APP;
 import static space.kiritron.duke_srv.ks_libs.pixel.tolchok.core.ReadCFG;
 
 public class Server extends Thread {
@@ -28,14 +28,14 @@ public class Server extends Thread {
 
     public void startServer() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            writeToConsole(true, true, NAME_APP, "Сервер запущен");
-            writeToConsole(true, true, NAME_APP, "Сервер использует порт - " + port);
+            toConsole.print(genLogMessage.gen((byte) 1, true,"Сервер запущен"));
+            toConsole.print(genLogMessage.gen((byte) 1, true, "Сервер использует порт - " + port));
             while (true) {
                 Socket socket = serverSocket.accept();
                 new space.kiritron.duke_srv.core.ServerThread(socket).start();
             }
         } catch (IOException ex) {
-            writeToConsole(true, true, NAME_APP, "ERROR: " + ex.getMessage());
+            toConsole.print(genLogMessage.gen((byte) 3, true, "ERROR: " + ex.getMessage()));
             ex.printStackTrace();
         }
     }
