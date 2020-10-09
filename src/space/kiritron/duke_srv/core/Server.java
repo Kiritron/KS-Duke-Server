18b1163cbc269062281df88030c77a5daf53e981
Kiritron Stablecore <kiritron@kiritron.space>
2020-10-09 19:57:48 +0300

    1.1. Библиотеки КС теперь отдельно от кода

--- src/space/kiritron/duke_srv/core/Server.java
+++ src/space/kiritron/duke_srv/core/Server.java
@@ -1,15 +1,31 @@
+/*
+ * Copyright 2020 Kiritron's Space
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.kiritron.duke_srv.core;
 
-import space.kiritron.duke_srv.ks_libs.pixel.filefunc.FileControls;
-import space.kiritron.duke_srv.ks_libs.pixel.filefunc.GetPathOfAPP;
-import space.kiritron.duke_srv.ks_libs.pixel.logger.genLogMessage;
-import space.kiritron.duke_srv.ks_libs.pixel.logger.toConsole;
+import space.kiritron.pixel.filefunc.FileControls;
+import space.kiritron.pixel.filefunc.GetPathOfAPP;
+import space.kiritron.pixel.logger.genLogMessage;
+import space.kiritron.pixel.logger.toConsole;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static space.kiritron.duke_srv.ks_libs.tolchok.TOLF_Handler.ReadParamFromData;
+import static space.kiritron.tolchok.TOLF_Handler.ReadParamFromData;
 
 public class Server extends Thread {
     protected static int port;
--- src/space/kiritron/duke_srv/core/ServerThread.java
+++ src/space/kiritron/duke_srv/core/ServerThread.java
@@ -1,15 +1,31 @@
+/*
+ * Copyright 2020 Kiritron's Space
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.kiritron.duke_srv.core;
 
-import space.kiritron.duke_srv.ks_libs.pixel.filefunc.GetPathOfAPP;
-import space.kiritron.duke_srv.ks_libs.pixel.logger.genLogMessage;
-import space.kiritron.duke_srv.ks_libs.pixel.logger.toConsole;
+import space.kiritron.pixel.filefunc.GetPathOfAPP;
+import space.kiritron.pixel.logger.genLogMessage;
+import space.kiritron.pixel.logger.toConsole;
 
 import java.io.*;
 import java.net.Socket;
 
 import static space.kiritron.duke_srv.init.NAME_APP;
-import static space.kiritron.duke_srv.ks_libs.pixel.filefunc.FileControls.ReadFile;
-import static space.kiritron.duke_srv.ks_libs.pixel.filefunc.FileControls.SearchFile;
+import static space.kiritron.pixel.filefunc.FileControls.ReadFile;
+import static space.kiritron.pixel.filefunc.FileControls.SearchFile;
 
 public class ServerThread extends Thread {
     private Socket socket;
